5d2efd33c19749982bd7bd314b007141a0f11bc3
Humberto Oliveira <hmoliveira88@gmail.com>
2025-08-31 12:39:39 +0100

    Minha página
    Trabalho final de curso de Html Css e JavaScript.

---
+++ Minha pagina/script.js
@@ -0,0 +1,12 @@
+
+// Alternar Modo Escuro
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+// Mostrar / Esconder Menu
+function toggleMenu() {
+    const menu = document.getElementById('linksNav');
+    menu.classList.toggle('hidden');
+}
+
